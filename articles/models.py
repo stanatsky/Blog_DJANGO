53fb0672ade4dc31929c2d70d88c76d7a3fdcd8a
twygw <stanley.dix@sky.uk>
2023-12-05 13:23:53 +0000

    created article db model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+# to add new field to the model use the following command:
+# python3 manage.py makemigrations
+# then type:
+# python3 manage.py migrate
+
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    # add thumbnail
+    # add author
+
+    def __str__(self):
+        return self.title
