ca488b328c6fd90cc4151f8187f35eb83be5fdc1
twygw <stanley.dix@sky.uk>
2023-12-06 11:30:41 +0000

    added images

--- articles/models.py
+++ articles/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
-    # add thumbnail
+    thumb = models.ImageField(default="default.png", upload_to="thumbnails")
     # add author
 
     def __str__(self):
