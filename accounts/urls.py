d710ca631cefee5f6f4c6579dda123208d6c11e1
twygw <stanley.dix@sky.uk>
2023-12-07 11:22:13 +0000

    added ability to log out

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
-    # path("login/", views.login_view, name="login"),
-    # path("logout/", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     # path("profile/", views.profile, name="profile"),
 ]
